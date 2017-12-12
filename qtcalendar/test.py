a8c99fcde785bdf556a6d06623792d7664901c68
Andrés Simancas Mateus <mateus@uninorte.edu.co>
2017-12-12 17:41:57 -0500

    Implemented generalized interface

--- qtcalendar/test.py
+++ qtcalendar/test.py
@@ -1,10 +1,12 @@
 from holder.minimal import MinimalHolder
-from views import EventInCalendar
+from calendar import EventInCalendar
 
 ui = MinimalHolder()
 
 # Create an event label
-label = EventInCalendar()
-ui.getMainLayout().addWidget(label)
+event = EventInCalendar()
+event.getView().setText('Hello, world!')
+event.getView().updateStatus()
+ui.getMainLayout().addWidget(event.getView())
 
 ui()
