2077026f4076c81e0620a58154454b4f9008e384
Andrés Simancas Mateus <mateus@uninorte.edu.co>
2017-12-13 18:18:23 -0500

    Created Date class and methods for inserting new events

--- qtcalendar/calendar.py
+++ qtcalendar/calendar.py
@@ -35,3 +35,38 @@ class EventInCalendar(Element):
         # Recursively set the event
         self._model.setEvent(event)
         self._view
+
+
+class Date(Element):
+    '''
+        Date class for the calendar. A calendar is basically a grid layout of multiple
+        date objects organized in a particular manner.
+        Date class holds the events that occur in a particular date and displays them
+        one on top of the other, organized by init_time.
+    '''
+
+    def __init__(self, date):
+        self._model = models.Date__Model(self, date)
+        self._view = views.Date__View(self)
+
+    def getView(self):
+        return self._view
+
+    def getModel(self):
+        return self._model
+
+    def addCalendarEvent(self, eic):
+        self._model.addEvent(eic)
+        self._view.updateFromModel()
+
+    def changeDate(self, new_date):
+        '''
+            Changes date of Date, this also resets the datetype to workdate, the inverse
+            (by datetype) does not occur
+        '''
+        self._model.setDate(new_date)
+        self._view.updateFromModel()
+
+    def changeDateType(self, datetype):
+        self._model.setDate(self._model.getDate(), datetype)
+        self._view.updateFromModel()
