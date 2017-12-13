028854d4856a8f3135585ccbcf98c26278d49814
Andrés Simancas Mateus <mateus@uninorte.edu.co>
2017-12-13 18:50:00 -0500

    Migrated to supermodels, now the view is not manipulated directly

--- qtcalendar/test.py
+++ qtcalendar/test.py
@@ -1,12 +1,18 @@
 from holder.minimal import MinimalHolder
-from calendar import EventInCalendar
+from calendar import EventInCalendar, Event
+import datetime as dt
 
 ui = MinimalHolder()
 
-# Create an event label
-event = EventInCalendar()
-event.getView().setText('Hello, world!')
-event.getView().updateStatus()
-ui.getMainLayout().addWidget(event.getView())
+# Create an Event
+event = Event(
+    dt.datetime(2017, 12, 13, 9, 0, 0),
+    dt.datetime(2017, 12, 13, 21, 0, 0))
 
+# Create an event entry (EventInCalendar)
+ecalendar = EventInCalendar()
+ecalendar.setEvent(event)
+ui.getMainLayout().addWidget(ecalendar.getView())
+
+# Start UI
 ui()
