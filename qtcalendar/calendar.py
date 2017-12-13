65f0c0c35743b397266b55f09e92c684af1f2c42
Andrés Simancas Mateus <mateus@uninorte.edu.co>
2017-12-13 16:58:18 -0500

    Added documentation and getEvent related functions

--- qtcalendar/calendar.py
+++ qtcalendar/calendar.py
@@ -2,13 +2,36 @@ import models
 import views
 
 
-class EventInCalendar:
+class Element:
+    pass
+
+
+class EventInCalendar(Element):
+    '''
+        EventInCalendar interface intended for usage in Date class, or for subclassing.
+        It contains model, view and controller of EventInCalendar:
+        * model: holds the information of EventInCalendar (displayed or not)
+        * view: visual classes and mechanisms
+        * controller: user interaction
+    '''
+
     def __init__(self):
-        self._model = models.EventInCalendar__Model()
+        self._model = models.EventInCalendar__Model(self)
         self._view = views.EventInCalendar__View(self)
+        self._event = None
 
     def getView(self):
         return self._view
 
     def getModel(self):
         return self._model
+
+    def getEvent(self):
+        return self._event
+
+    def setEvent(self, event):
+        self._event = event
+
+        # Recursively set the event
+        self._model.setEvent(event)
+        self._view
