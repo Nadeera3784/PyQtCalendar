de98b347bb0c3b7e7894e9016783f575b1e6380c
Andrés Simancas Mateus <mateus@uninorte.edu.co>
2017-12-19 15:16:07 -0500

    Created Calendar controller and spanshot displacement function

--- qtcalendar/controllers.py
+++ qtcalendar/controllers.py
@@ -1,3 +1,32 @@
 '''
     User input management for Calendar Widgets
 '''
+from PyQt5 import QtGui, QtCore
+
+
+class Calendar__Controller:
+    '''
+        Controller for defining actions upon user interaction. The calendar itself
+        allows the user 3 kinds of actions (as of yet):
+         * snapshot switching
+         * date selection
+         * event creation
+    '''
+
+    def __init__(self, master):
+        self._master = master
+
+    def handleEvent(self, event):
+        if isinstance(event, QtGui.QMouseEvent):
+            if event.button() == QtCore.Qt.LeftButton:
+                self.displaceCalendarSnapshot(-1)
+            elif event.button() == QtCore.Qt.RightButton:
+                self.displaceCalendarSnapshot(1)
+
+    def displaceCalendarSnapshot(self, direction):
+        if direction is -1:
+            new_month = self._master.getModel().monthSubtract()
+            self._master.getModel().setMonth(new_month)
+        elif direction is 1:
+            new_month = self._master.getModel().monthAdd()
+            self._master.getModel().setMonth(new_month)
